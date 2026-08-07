c3b8666566aa5a575bb8bbd99b7a874b3fe70dfa
Sercan <sercansimsek44@gmail.com>
2026-08-07 13:57:26 +0200

    Header section created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { Button } from "./components/Button";
+
 export default function App() {
-  return <div className="mt-4">App</div>
+  return (
+    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-3xl font-bold">Habit Tracker</h1>
+        <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+      </div>
+      <div className="flex flex-col gap-1 items-end">
+        <span>Apr - Apr 12</span>
+        <div className="flex items-center gap-4">
+          <Button>Prev</Button>
+          <Button>Next</Button>
+        </div>
+      </div>
+    </header>
+  );
 }
